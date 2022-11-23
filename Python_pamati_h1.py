260e43f59dfbca62b4822ace8d1cec9ff39c306b
Andris Andersons <andersonsandris890@gmail.com>
2022-11-23 20:12:32 +0200

    test

---
+++ Python_pamati_h1.py
@@ -0,0 +1,6 @@
+vards = input("Ievadi savu vardu: ")
+uzvards = input("Ievadi savu uzvardu: ")
+
+print(vards.upper(), uzvards.upper())
+print(vards.lower(), uzvards.lower())
+print(vards.title(), uzvards.title())
