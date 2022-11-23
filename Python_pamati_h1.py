2f2c8f045166c88e3b38cb4267374d047e9044c4
Andris Andersons <andersonsandris890@gmail.com>
2022-11-23 22:44:33 +0200

    Deleted

--- Python_pamati_h1.py
+++
@@ -1,6 +0,0 @@
-vards = input("Ievadi savu vardu: ")
-uzvards = input("Ievadi savu uzvardu: ")
-
-print(vards.upper(), uzvards.upper())
-print(vards.lower(), uzvards.lower())
-print(vards.title(), uzvards.title())
